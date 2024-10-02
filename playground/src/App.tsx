2ac35d26b81aa38b74bfc388a0e304c456a60888
hunghg255 <giahunghust@gmail.com>
2024-10-02 14:40:03 +0700

    chore: update base css

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -1,22 +1,23 @@
 /* eslint-disable no-console */
-import './App.css'
-import { useState } from 'react'
+import './App.css';
+import { useState } from 'react';
 
-import Modal from '@rc-ui/dialog'
-import { Picker, PickerPanel, RangePicker } from '@rc-ui/picker'
+import Modal from '@rc-ui/dialog';
+import { Picker, PickerPanel, RangePicker } from '@rc-ui/picker';
 
-import enUS from 'rc-picker/lib/locale/en_US'
-import 'dayjs/locale/vi'
+import enUS from 'rc-picker/lib/locale/en_US';
+import 'dayjs/locale/vi';
 
-import '@rc-ui/dialog/style.css'
-import '@rc-ui/picker/style.css'
+import '@rc-ui/base/style.css';
+import '@rc-ui/dialog/style.css';
+import '@rc-ui/picker/style.css';
 
 function App() {
-  const [visible, setVisible] = useState(false)
+  const [visible, setVisible] = useState(false);
 
   function disabledDate(current: any) {
     // Can not select days before today and today
-    return current && current.valueOf() < Date.now()
+    return current && current.valueOf() < Date.now();
   }
 
   return (
@@ -37,36 +38,36 @@ function App() {
       <div>
         <Picker
           locale={enUS}
-          format="DD/MM/YYYY"
+          format='DD/MM/YYYY'
           onChange={(v) => {
-            console.log(v)
+            console.log(v);
           }}
           suffixIcon={<span>suffix</span>}
         />
-        <Picker locale={enUS} picker="week" />
-        <Picker locale={enUS} picker="month" />
-        <Picker locale={enUS} picker="quarter" />
-        <Picker locale={enUS} picker="year" />
+        <Picker locale={enUS} picker='week' />
+        <Picker locale={enUS} picker='month' />
+        <Picker locale={enUS} picker='quarter' />
+        <Picker locale={enUS} picker='year' />
 
-        <Picker locale={enUS} picker="time" />
+        <Picker locale={enUS} picker='time' />
 
         <RangePicker
           locale={enUS}
           disabledDate={disabledDate}
           onChange={(v) => {
-            console.log(v)
+            console.log(v);
           }}
         />
         <PickerPanel
           locale={enUS}
           disabledDate={disabledDate}
           onChange={(v) => {
-            console.log('a', v)
+            console.log('a', v);
           }}
         />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
